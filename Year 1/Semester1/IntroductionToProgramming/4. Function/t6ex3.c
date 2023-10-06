4f3096c9dd62b808186dc89dacbae6f1ca9ff7b4
Ruvindu Amararathna <116617572+RuviDev@users.noreply.github.com>
2023-10-06 11:51:26 +0530

    Added function folder

---
+++ Year 1/Semester1/IntroductionToProgramming/4. Function/t6ex3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+//float circleArea(float radius);
+//float circlePeri(float radius);
+//float recArea(float width, float length);
+
+float circleArea(float radius){
+	float area = 22/7.0 * radius * radius;
+	return area;
+}
+
+float circlePeri(float radius){
+	float perimeter = 2.0 * 22/7.0 * radius;
+	return perimeter;
+}
+
+float recArea(float width, float length){
+	float rec_area = length * width;
+	return rec_area; 
+}
+
+int main(void){
+	float radius, height, cyl_area;
+	
+	printf("Enter the radius : ");
+	scanf("%f", &radius);
+	printf("Enter the height : ");
+	scanf("%f", &height);
+	
+//	cyl_area = circlePeri(radius) * height + (2.0 * circleArea(radius));
+	cyl_area = recArea(height, circlePeri(radius)) + (2.0 * circleArea(radius));
+	
+	printf("Cylinder Area : %.2f", cyl_area);
+	
+	return 0;
+}
+
+
